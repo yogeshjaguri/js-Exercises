b2287a81d0ff7e9d955d5a0a4968bc0ec8fd69b5
Yogesh <yogesh.jaguri2401@gmail.com>
2023-07-10 17:18:45 +0530

    my 3rd commit

--- errors.js
+++ errors.js
@@ -27,3 +27,11 @@ try {
 }
 
 // Write a JavaScript function that accepts two numbers as parameters and throws a custom 'Error' if the second number is zero.
+function divide_Numbers(n1, n2) {
+  if (n2 === 0) {
+    throw new Error("Error: Division by zero is not allowed.");
+  }
+  return n1 / n2;
+}
+console.log(divide_Numbers(8, 3));
+console.log(divide_Numbers(8, 0));
